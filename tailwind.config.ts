0087c8074d0f9292c1242372971449c604e22354
kisanet shshay <shshaykisanet@gmail.com>
2025-09-25 19:13:12 +0300

    fix: fixed merge conflicts

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          nunito: ['Nunito', 'sans-serif'],
+        },
+        colors: {
+          'custom-green': '#234B06',
+          'orangeCustom': '#FF8614',
+        },
+       
+      },
+    },
+    plugins: [
+  
+      require("tailwind-scrollbar"),   
+    ],
+  };
+  
